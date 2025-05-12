cf6eebdd52f5dbda207a787156d777f839b3bd6b
Vitali Karabchuk <v.karabchuk@wakala.it>
2025-05-12 11:44:03 +0200

    Enable library evolution

--- Package.swift
+++ Package.swift
@@ -15,8 +15,9 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Factory",
-            targets: ["Factory"]),
+            name: "FactoryKit",
+            type: .dynamic,
+            targets: ["FactoryKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -26,11 +27,30 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Factory",
+            name: "FactoryKit",
             dependencies: [],
-            resources: [.copy("PrivacyInfo.xcprivacy")]),
+            resources: [.copy("PrivacyInfo.xcprivacy")],
+//            swiftSettings: [
+//                // Emulate BUILD_LIBRARY_FOR_DISTRIBUTION to prevent culling of ABI used by
+//                // compiled frameworks.
+//                .unsafeFlags([
+//                    "-enable-library-evolution",
+//                    "-emit-module-interface"
+//                ]),
+//            ]
+        ),
         .testTarget(
             name: "FactoryTests",
-            dependencies: ["Factory"]),
+            dependencies: ["FactoryKit"]),
     ]
 )
+
+for target in package.targets where target.type != .system {
+    target.swiftSettings = target.swiftSettings ?? []
+    target.swiftSettings?.append(contentsOf: [
+        .unsafeFlags([
+            "-enable-library-evolution",
+            "-emit-module-interface"
+        ]),
+    ])
+}
